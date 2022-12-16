76a087b3d23ae77a98ab2a6f007a5311a1bd0bbb
FontaMatte <matteo.fontanel@gmail.com>
2022-12-16 16:30:29 +0100

    Excercise completed

--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,17 @@ const userFavColor = prompt("Qual'e il tuo colore preferito?");
 console.log("il colore preferito dell'utente e': ",userFavColor);
 
 // richiesta numero preferito
-const userFavNumber = prompt("Qual'e il tuo numero preferito?");
-console.log("il numero preferito dell'utente e': ",userFavNumber);
+// const userFavNumber = prompt("Qual'e il tuo numero preferito?");
+// console.log("il numero preferito dell'utente e': ",userFavNumber);
 
 // stampo l'intera stringa sull'HTML
-// let userFavNumber = 21;
-// let userInfo = userName + userSurname + userFavColor + userFavNumber;
-// console.log(userInfo);
+let userFavNumber = 21;
+let userInfo = userName + userSurname + userFavColor + userFavNumber;
+console.log(userInfo);
 
-// document.getElementById('userInfo').innerHTML = userInfo ;
+document.getElementById('userInfo').innerHTML = userInfo ;
 
-document.getElementById('userName').innerHTML = "Nome:" + " " + userName ;
-document.getElementById('userSurname').innerHTML = "Cognome:" + " " + userSurname ;
-document.getElementById('userFavColor').innerHTML = "Colore preferito:" + " " + userFavColor ;
-document.getElementById('userFavNumber').innerHTML = "Numero preferito:" + " " + userFavNumber;
+// document.getElementById('userName').innerHTML = "Nome:" + " " + userName ;
+// document.getElementById('userSurname').innerHTML = "Cognome:" + " " + userSurname ;
+// document.getElementById('userFavColor').innerHTML = "Colore preferito:" + " " + userFavColor ;
+// document.getElementById('userFavNumber').innerHTML = "Numero preferito:" + " " + userFavNumber;
