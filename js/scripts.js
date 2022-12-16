4e987a26a4b2951e75b5166f7090114d3b0477e9
FontaMatte <matteo.fontanel@gmail.com>
2022-12-16 15:42:37 +0100

    Bonus: print results separate inside a ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 // richiesta nome utente
 const userName = prompt("Qual'e il tuo nome?");
-console.log("l'utente si chiama: ",userName)
+console.log("l'utente si chiama: ",userName);
 
 // richiesta cognome utente
 const userSurname = prompt("Qual'e il tuo cognome?");
-console.log("il cognome dell'utente e': ",userSurname)
+console.log("il cognome dell'utente e': ",userSurname);
 
 
 // richiesta colore preferito
 const userFavColor = prompt("Qual'e il tuo colore preferito?");
-console.log("il colore preferito dell'utente e': ",userFavColor)
+console.log("il colore preferito dell'utente e': ",userFavColor);
+
+// richiesta numero preferito
+const userFavNumber = prompt("Qual'e il tuo numero preferito?");
+console.log("il numero preferito dell'utente e': ",userFavNumber);
 
 // stampo l'intera stringa sull'HTML
-let userFavNumber = 21;
-let userInfo = userName + userSurname + userFavColor + userFavNumber;
-console.log(userInfo);
+// let userFavNumber = 21;
+// let userInfo = userName + userSurname + userFavColor + userFavNumber;
+// console.log(userInfo);
 
-document.getElementById('userInfo').innerHTML = userInfo ;
+// document.getElementById('userInfo').innerHTML = userInfo ;
+
+document.getElementById('userName').innerHTML = "Nome:" + " " + userName ;
+document.getElementById('userSurname').innerHTML = "Cognome:" + " " + userSurname ;
+document.getElementById('userFavColor').innerHTML = "Colore preferito:" + " " + userFavColor ;
+document.getElementById('userFavNumber').innerHTML = "Numero preferito:" + " " + userFavNumber;
